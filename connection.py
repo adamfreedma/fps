f823753b116eedbddfb35c717db15f05d7c35ae0
adamfreedma <adamfreedma@gmail.com>
2022-03-09 16:34:17 +0200

    Fixed encode/decode and added more alaborated exit messages.

--- connection.py
+++ connection.py
@@ -9,36 +9,37 @@ class Connection:
         self.SERVER_PORT = server_port
         # connecting to the server
         self.client_socket = socket.socket()
+        self.client_socket.settimeout(0.1)
         try:
             self.client_socket.connect((self.SERVER_IP, self.SERVER_PORT))
         except socket.error:
-            exit("server offline")
+            exit("server offline, init")
 
     def connect(self):
         try:
-            self.client_socket.send("J")
+            self.client_socket.send("J".encode())
             init_data = self.client_socket.recv(1024).decode()
-            if init_data[0] != "S":
+            if len(init_data) < 1 or init_data[0] != "S":
                 raise socket.error
             return deserialize_new_player(init_data[1:])
         except socket.error:
-            exit("server offline")
+            exit("server offline, connect")
     
     def send_movement(self, player):
         try:
-            self.client_socket.send(("U" + player.serialize()).encode())
+            self.client_socket.send(("P" + player.serialize()).encode())
         except socket.error:
-            exit("server offline")
+            exit("server offline, send movement")
 
 
     def recv_updates(self):
         try:
-            update_data = self.client_socket.recv(1024)
+            update_data = self.client_socket.recv(1024).decode()
             if update_data[0] == "H":
                 return update_data[0:], None
             elif update_data[0] == "P":
                 p = deserialize_player(update_data[0:])
                 return p.color. p
         except socket.error:
-            exit("server offline")
+            exit("server offline, recv updates")
 
