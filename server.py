273b8628388cddf4150bb532953f63569d087a73
adamfreedma <adamfreedma@gmail.com>
2022-04-08 17:49:55 +0300

    splits to player and points

--- server.py
+++ server.py
@@ -23,16 +23,14 @@ class Game:
         self.message_to_send = []
 
         self.RESPAWN_TIME = 5
-        self.POINTS_TO_WIN = 10
+        self.POINTS_TO_WIN = 9
 
     def _disconnect_all_clients(self):
         for client_socket in self.open_client_sockets:
-            self.color_list[self.socket_colors[client_socket]] = True
-            del self.player_list[self.socket_colors[client_socket]]
-            del self.socket_colors[client_socket]
-            print(f'{self.open_client_sockets[client_socket]} - disconnected')
-            del self.open_client_sockets[client_socket]
             client_socket.close()
+        self.player_list = {}
+        self.socket_colors = {}
+        self.open_client_sockets = {}
 
 
     def _handle_disconnected_client(self, client_socket):
@@ -47,13 +45,8 @@ class Game:
 
     def _reset_vars(self):
         self.points = {}
-        self.socket_colors = {}
         self.color_list = {"green": True, "red": True, "blue": True}
-        self.player_list = {}
         self.death_times = {}
-        self.open_client_sockets = {}
-        self.message_to_send = []
-
 
     def _send_waiting_messages(self, wlist, messages):
         for message in messages:
@@ -107,7 +100,9 @@ class Game:
                                 player.move_to(x=randint(-10, 10), z=randint(-10, 10))
                                 self.player_list[hit] = player.serialize()[:50]
                                 self.death_times[hit] = time()
+                                print(self.points[self.socket_colors[curr_socket]])
                                 self.points[self.socket_colors[curr_socket]] += 1
+                                print(self.points[self.socket_colors[curr_socket]])
                                 for target_socket in w_list:
                                     self.message_to_send.append((target_socket, self.player_list[hit] + hit + "P"))
 
@@ -116,14 +111,15 @@ class Game:
                                     message = ""
                                     # sorting the self.points list from most self.points to least
                                     self.points = {k: v for k, v in sorted(self.points.items(), key=lambda item: item[1], reverse=True)}
+                                    self.message_to_send = []
                                     for key, val in self.points.items():
-                                        message += "F" + key + val
-                                        self.message_to_send = []
+                                        message += "F" + key + str(val).zfill(1)
 
                                     for target_socket in w_list:
                                         self.message_to_send.append((target_socket, message))
-                                    self._disconnect_all_clients()
+                                    self._send_waiting_messages(w_list, self.message_to_send)
                                     self._reset_vars()
+                                    
 
 
                         elif data[0] == "P":
@@ -134,7 +130,8 @@ class Game:
                                     # [i] position in gl cs
                                     value = player_data[:50]
                                     self.player_list[color] = value
-                                    self.points[color] = 0
+                                    if color not in self.points:
+                                        self.points[color] = 0
                                     server_objects.create_player(color, Player.deserialize_player(value).position)
                             message = ""
                             for key, val in self.player_list.items():
@@ -147,8 +144,7 @@ class Game:
 
 def main():
     game = Game()
-    while True:
-        game.play()
+    game.play()
 
 
 if __name__ == "__main__":
