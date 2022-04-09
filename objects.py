ac736c4b46bbe01a02e004a51d113535e392a57c
adamfreedma <adamfreedma@gmail.com>
2022-04-09 22:35:04 +0300

    Created map

--- objects.py
+++ objects.py
@@ -19,18 +19,203 @@ class Object:
 
 
 colors = {"black": [0, 0, 0], "green": [0, 1, 0], "red": [1, 0, 0], "blue": [0, 0, 1]}
+
+brown = (0.43, 0.3, 0.21)
+gray = (0.4, 0.4, 0.4)
+
 # [i] object cs
-cube_vertices_vector3 = [
-    [1.0000, 1.0000, 1.0000],
-    [1.0000, 1.0000, -1.0000],
-    [1.0000, -1.0000, 1.0000],
-    [1.0000, -1.0000, -1.0000],
-    [-1.0000, 1.0000, 1.0000],
-    [-1.0000, 1.0000, -1.0000],
-    [-1.0000, -1.0000, 1.0000],
-    [-1.0000, -1.0000, -1.0000]]
+# *all boxes vertices*
+b1_vertices_vector3 = [
+    [5,-20,-1],
+    [5,-20,1],
+    [5,10,-1],
+    [5,10,1],
+    [25,-20,-1],
+    [25,-20,1],
+    [25,10,-1],
+    [25,10,1],
+]
+
+b2_vertices_vector3 = [
+    [5,5,-1],
+    [5,5,1],
+    [5,10,-1],
+    [5,10,1],
+    [-20,5,-1],
+    [-20,5,1],
+    [-20,10,-1],
+    [-20,10,1],
+]
+
+a1_vertices_vector3 = [
+    [-5,0,-1],
+    [-5,0,1],
+    [-5,-5,-1],
+    [-5,-5,1],
+    [-35,0,-1],
+    [-35,0,1],
+    [-35,-5,-1],
+    [-35,-5,1],
+]
+
+a2_vertices_vector3 = [
+    [-15,-5,-1],
+    [-15,-5,1],
+    [-15,-15,-1],
+    [-15,-15,1],
+    [-35,-5,-1],
+    [-35,-5,1],
+    [-35,-15,-1],
+    [-35,-15,1],
+]
+
+c_vertices_vector3 = [
+    [25,20,-1],
+    [25,20,1],
+    [25,40,-1],
+    [25,40,1],
+    [5,20,-1],
+    [5,20,1],
+    [5,40,-1],
+    [5,40,1],
+]
+
+d_vertices_vector3 = [
+    [0,40,-1],
+    [0,40,1],
+    [0,30,-1],
+    [0,30,1],
+    [-20,40,-1],
+    [-20,40,1],
+    [-20,30,-1],
+    [-20,30,1],
+]
+
+e_vertices_vector3 = [
+    [0,25,-1],
+    [0,25,1],
+    [0,15,-1],
+    [0,15,1],
+    [-20,25,-1],
+    [-20,25,1],
+    [-20,15,-1],
+    [-20,15,1],
+]
+
+
+f_vertices_vector3 = [
+    [0,-10,-1],
+    [0,-10,1],
+    [0,-15,-1],
+    [0,-15,1],
+    [-5,-10,-1],
+    [-5,-10,1],
+    [-5,-15,-1],
+    [-5,-15,1],
+]
+
+w1_vertices_vector3 = [
+    [-15,-25,-1],
+    [-15,-25,1],
+    [-15,-26,-1],
+    [-15,-26,1],
+    [-30,-25,-1],
+    [-30,-25,1],
+    [-30,-26,-1],
+    [-30,-26,1],
+]
+
+w2_vertices_vector3 = [
+    [-10,-10,-1],
+    [-10,-10,1],
+    [-10,-26,-1],
+    [-10,-26,1],
+    [-11,-10,-1],
+    [-11,-10,1],
+    [-11,-26,-1],
+    [-11,-26,1],
+]
+
+w3_vertices_vector3 = [
+    [25,-25,-1],
+    [25,-25,1],
+    [25,-26,-1],
+    [25,-26,1],
+    [0,-25,-1],
+    [0,-25,1],
+    [0,-26,-1],
+    [0,-26,1],
+]
+
+
+wt_vertices_vector3 = [
+    [30,50,-1],
+    [30,50,1],
+    [30,-31,-1],
+    [30,-31,1],
+    [31,50,-1],
+    [31,50,1],
+    [31,-31,-1],
+    [31,-31,1],
+]
+
+wb1_vertices_vector3 = [
+    [-30,50,-1],
+    [-30,50,1],
+    [-30,5,-1],
+    [-30,5,1],
+    [-31,50,-1],
+    [-31,50,1],
+    [-31,5,-1],
+    [-31,5,1],
+]
+wb2_vertices_vector3 = [
+    [-45,-31,-1],
+    [-45,-31,1],
+    [-45,5,-1],
+    [-45,5,1],
+    [-46,-31,-1],
+    [-46,-31,1],
+    [-46,5,-1],
+    [-46,5,1],
+]
+
+wr_vertices_vector3 = [
+    [30,-30,-1],
+    [30,-30,1],
+    [30,-31,-1],
+    [30,-31,1],
+    [-46,-30,-1],
+    [-46,-30,1],
+    [-46,-31,-1],
+    [-46,-31,1],
+]
+
+wl1_vertices_vector3 = [
+    [31, 50,-1],
+    [31, 50,1],
+    [31, 51,-1],
+    [31, 51,1],
+    [-31,50,-1],
+    [-31,50,1],
+    [-31,51,-1],
+    [-31,51,1],
+]
+
+wl2_vertices_vector3 = [
+    [-30,5,-1],
+    [-30,5,1],
+    [-30,4,-1],
+    [-30,4,1],
+    [-46,5,-1],
+    [-46,5,1],
+    [-46,4,-1],
+    [-46,4,1],
+]
+
+
 # [i] object cs
-cube_edges_vector2 = [
+edges_vector2 = [
     [5, 7],
      [1, 5],
      [0, 1],
@@ -45,7 +230,7 @@ cube_edges_vector2 = [
      [3, 1],
     ]
 # [i] object cs
-cube_faces_vector4 = [
+faces_vector4 = [
     [0, 4, 6, 2],
     [3, 2, 6, 7],
     [7, 6, 4, 5],
@@ -55,14 +240,15 @@ cube_faces_vector4 = [
     ]
 # [i] object cs
 floor_vertices_vector3 = [
+    [100.0000, 100.0000, -1.0000],
     [100.0000, 100.0000, -2.0000],
-    [100.0000, 100.0000, -3.0000],
+    [100.0000, -100.0000, -1.0000],
     [100.0000, -100.0000, -2.0000],
-    [100.0000, -100.0000, -3.0000],
+    [-100.0000, 100.0000, -1.0000],
     [-100.0000, 100.0000, -2.0000],
-    [-100.0000, 100.0000, -3.0000],
-    [-100.0000, -100.0000, -2.0000],
-    [-100.0000, -100.0000, -3.0000],]
+    [-100.0000, -100.0000, -1.0000],
+    [-100.0000, -100.0000, -2.0000],]
+
 # [i] object cs
 floor_edges_vector2 = [
     [5, 7],
@@ -124,7 +310,25 @@ player_template_faces_vector4 = [
 # [i] object cs
 players = {}
 # [i] object cs
-world = [Object(cube_faces_vector4, cube_vertices_vector3, cube_edges_vector2), Object(floor_faces_vector4, floor_vertices_vector3, floor_edges_vector2, [0.3,0.3,0.3])]
+world = [Object(faces_vector4, a1_vertices_vector3, edges_vector2, gray), 
+         Object(faces_vector4, a2_vertices_vector3, edges_vector2, gray), 
+         Object(faces_vector4, b1_vertices_vector3, edges_vector2, gray), 
+         Object(faces_vector4, b2_vertices_vector3, edges_vector2, gray),
+         Object(faces_vector4, c_vertices_vector3, edges_vector2, gray),
+         Object(faces_vector4, d_vertices_vector3, edges_vector2, gray),
+         Object(faces_vector4, e_vertices_vector3, edges_vector2, gray),
+         Object(faces_vector4, f_vertices_vector3, edges_vector2, gray), 
+         Object(faces_vector4, w1_vertices_vector3, edges_vector2, gray), 
+         Object(faces_vector4, w2_vertices_vector3, edges_vector2, gray), 
+         Object(faces_vector4, w3_vertices_vector3, edges_vector2, gray),
+         Object(faces_vector4, wt_vertices_vector3, edges_vector2, gray),
+         Object(faces_vector4, wb1_vertices_vector3, edges_vector2, gray),
+         Object(faces_vector4, wb2_vertices_vector3, edges_vector2, gray),
+         Object(faces_vector4, wr_vertices_vector3, edges_vector2, gray),
+         Object(faces_vector4, wl1_vertices_vector3, edges_vector2, gray),
+         Object(faces_vector4, wl2_vertices_vector3, edges_vector2, gray),
+         Object(floor_faces_vector4, floor_vertices_vector3, floor_edges_vector2, brown),
+]
 
 
 def create_player(color, position, is_gl_cs=True):
